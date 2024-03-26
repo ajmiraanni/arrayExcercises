1f68c29f528ea98c98ea39531edc5376c7474468
ajmiraanni <ajmira.anni@gmail.com>
2024-03-26 11:20:38 +1100

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import {
+    fillArray
+} from './exercises';
+
+// All work should be placed in a separate file/module called
+// exercises.ts
+fillArray();
+//orderPizza();
+//findAverageScore();
+//findEvenNumbers();
+//findNumberCount();
