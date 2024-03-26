b0d2d9857e67a4cd1f22a638b274661fd1253674
ajmiraanni <ajmira.anni@gamil.com>
2024-03-26 11:41:01 +1100

    compleated task one

--- src/exercises.ts
+++ src/exercises.ts
@@ -1,5 +1,18 @@
 export function fillArray() : void {
     // Answer the question here
+
+    let numbers: number[] = new Array(10);
+    //fill the array with 10 random interger numbers between 0 and 70
+    for(let i = 0; i < numbers.length; i++){
+        numbers[i] = Math.floor(Math.random() * 71);
+    }
+
+    //display the integer number from first to last index positon
+    console.log("Numbers form first to last: ", numbers);
+
+    //display the integer numbers from last to first index position
+    let reverseNumbers : number[] = [...numbers].reverse(); 
+    console.log("Numbers from last to first: ", reverseNumbers);
 }
 
 function getRandomInt(min: number, max: number) : number {
