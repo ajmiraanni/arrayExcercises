13bb8a72624f30e2d9ce209c52aa378ac5676686
ajmiraanni <ajmira.anni@gamil.com>
2024-03-26 12:17:22 +1100

    completed task two

--- src/exercises.ts
+++ src/exercises.ts
@@ -1,3 +1,5 @@
+// import * as readline from 'readline';
+import * as readline from 'readline-sync';
 export function fillArray() : void {
     // Answer the question here
 
@@ -13,6 +15,25 @@ export function fillArray() : void {
     //display the integer numbers from last to first index position
     let reverseNumbers : number[] = [...numbers].reverse(); 
     console.log("Numbers from last to first: ", reverseNumbers);
+}
+
+export function orderPizza() : void{
+
+    let pizzaSize: string = readline.question("Enter the size of the pizza (S,M,L or X):");
+    switch(pizzaSize){
+        case 'S':
+            console.log("The price is $6.99");
+            break;
+        case 'M':
+            console.log("The price is $8.99");
+            break;
+        case 'L':
+            console.log("The price is $12.50");
+            break;
+        case 'X':
+            console.log("The price is $15.00");
+            break;
+    }
 }
 
 function getRandomInt(min: number, max: number) : number {
